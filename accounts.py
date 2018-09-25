72eb4d242dff15c500aa969a0da6c169613d97b8
armstrongsouljah <armstrongsouljah@gmail.com>
2018-09-26 00:42:21 +0300

    Add travis config

--- accounts.py
+++ accounts.py
@@ -11,4 +11,11 @@ class Account:
             email=email,
             password=password
         )
-        return 
+        if len(self.account['password']) < 4:
+            raise ValueError("Password should be atleast 4 characters and above")
+        
+        if not isinstance(self.account['age'], int) and self.account['age'] < 0:
+            raise ValueError("Age must be a number and not equal to or less than 0")
+
+        if self.account not in self.accounts:
+            self.accounts.append(self.account)
--- test_accounts.py
+++ test_accounts.py
@@ -5,3 +5,16 @@ from accounts import Account
 class TestAccount(unittest.TestCase):
     def setUp(self):
         self.account = Account()
+        self.accounts = self.account.accounts
+        self.sample_account = dict(
+            name = "armstrong",
+            username = "soultech",
+            age = 24,
+            email = "armstrongsouljah@gmail.com",
+            password = "Aa9s0!*"
+        )
+
+    def test_user_can_reigster(self):
+        self.assertEqual(len(self.accounts), 0)
+        self.account.register_user(**self.sample_account)
+        self.assertEqual(len(self.accounts), 1)
