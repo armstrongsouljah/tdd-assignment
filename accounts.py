80381e875c53b761f6e0c8c097c0f3b09534698c
armstrongsouljah <armstrongsouljah@gmail.com>
2018-09-26 02:06:27 +0300

    Test user registration

--- accounts.py
+++ accounts.py
@@ -3,19 +3,32 @@ class Account:
     def __init__(self):
         self.accounts = []
 
-    def register_user(self, name, username, age, email, password):
+    def register_user(self, name, username, age, email, password, gender):
         self.account = dict(
             name=name,
             username=username,
             age=age,
             email=email,
-            password=password
+            password=password,
+            gender=gender
         )
         if len(self.account['password']) < 4:
             raise ValueError("Password should be atleast 4 characters and above")
         
-        if not isinstance(self.account['age'], int) and self.account['age'] < 0:
+        if self.account['gender'] != "female" and self.account['gender'] !="male":
+            raise ValueError("Gender should be male / female!")
+        
+        if not isinstance(self.account['age'], int) and self.account['age'] <= 0:
             raise ValueError("Age must be a number and not equal to or less than 0")
+
+        if self.account['username'] == self.account['name']:
+            raise ValueError("Username should be different from name")
+
+        if self.account['username'] and len(self.account['username']) < 4:
+            raise ValueError("Username should be atleast 4 characters and above")
+
+
+        
 
         if self.account not in self.accounts:
             self.accounts.append(self.account)
--- test_accounts.py
+++ test_accounts.py
@@ -11,10 +11,54 @@ class TestAccount(unittest.TestCase):
             username = "soultech",
             age = 24,
             email = "armstrongsouljah@gmail.com",
-            password = "Aa9s0!*"
+            password = "Aa9s0!*",
+            gender = "male"
+        )
+
+        self.sample_account2 = dict(
+            name = "soultech",
+            username = "soultech",
+            age = 24,
+            email = "armstrongsouljah@gmail.com",
+            password = "Aa9s0!*",
+            gender = "male"
+        )
+
+        self.sample_account3 = dict(
+            name = "pauljonas",
+            username = "kyx",
+            age = 24,
+            email = "armstrongsouljah@gmail.com",
+            password = "Aa9s0!*",
+            gender = "male"
+        )
+
+        self.sample_account4 = dict(
+            name = "pauljonas",
+            username = "kyx",
+            age = 0,
+            email = "armstrongsouljah@gmail.com",
+            password = "Aa9s0!*",
+            gender = "male"
         )
 
     def test_user_can_reigster(self):
         self.assertEqual(len(self.accounts), 0)
         self.account.register_user(**self.sample_account)
         self.assertEqual(len(self.accounts), 1)
+
+    def test_username_not_similar_to_name(self):
+        with self.assertRaises(ValueError) as context:
+            self.account.register_user(**self.sample_account2)
+            self.assertTrue("Username should be different from name" in context.exception)
+
+    def test_username_not_below_4_characters(self):
+        with self.assertRaises(ValueError) as context:
+            self.account.register_user(**self.sample_account3)
+            self.assertTrue("Username should be atleast 4 characters and above" in context.exception)
+
+    def test_age_should_be_number(self):
+        with self.assertRaises(ValueError) as context:
+            self.account.register_user(**self.sample_account4)
+            self.assertTrue("Age must be a number and not equal to or less than 0" in context.exception)
+        
